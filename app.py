52241e956ce17feeee9417e5898f807bc4ec78e2
JuarezCosta <juarezcostasilva1999@outlook.com>
2025-08-08 00:20:14 -0300

    Create app.py
    primeiros passos do curso de python na Alura

---
+++ app.py
@@ -0,0 +1,10 @@
+
+print('ℙ𝕚𝕫𝕫𝕒 ℙ𝕝𝕒𝕟𝕖𝕥')
+print("""
+[1] Cadastrar restaurante
+[2] Listar restaurante
+[3] Ativar restaurante
+[4] Sair""")
+
+opção = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opção}')
