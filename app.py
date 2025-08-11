fc6482e4daa55780a2a6e1b73e5d462e98dbe111
JuarezCosta <juarezcostasilva1999@outlook.com>
2025-08-11 13:00:57 -0300

    feature de sair do app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 
 print('ℙ𝕚𝕫𝕫𝕒 ℙ𝕝𝕒𝕟𝕖𝕥')
 print("""
@@ -6,5 +7,20 @@ print("""
 [3] Ativar restaurante
 [4] Sair""")
 
-opção = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opção}')
+#ENTRADA DE DADOS
+opção = int(input('Escolha uma opção: '))
+
+#ENCERRA O PROGRAMA
+def finalizar_app():
+    os.system('cls')
+    print("Encerrando App...\n")
+
+#CONDIÇÃO
+if opção == 1:
+    print('Cadastrar restaurante')
+elif opção == 2:
+    print('Listar restaurantes')
+elif opção == 3: 
+    print('Ativar restaurante')
+else:
+    finalizar_app()
